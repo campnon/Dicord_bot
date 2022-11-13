2145abdb52c021f9b81c4727f1891c8da6730584
CampNoNet <bob92588@gmail.com>
2022-11-13 12:31:38 -0500

    Init Commit

--- discord_bot/guess.py
+++ discord_bot/guess.py
@@ -1,14 +1,26 @@
 import random 
+import json
+import os 
 
 class GameGuess:
     def __init__(self, message):
-        self.user = massage.author
-        self.message = message.content
+        #self.user = message.author
+        pass
+        #self.message = message.content
+
+   
+    def open_file(slef):
+        fhand  = 'data.json'
+        fopen = open(fhand, 'w')
+        data = json.load(fopen)
+        return data
 
     def pick_random(self, min, max):
         self.min = min
         self.max = max
         choice = random.randint(self.min, self.max)
+        save = 'not code yet will be a function to save' 
+        #the guess witht he username of the perosn who made it'
         return choice
 
     def game(self):
@@ -16,4 +28,5 @@ class GameGuess:
     
     def test(self):
         a = stock.get_symbol(self.message)
-   
+    
+GameGuess("test").pick_random(0, 100)
