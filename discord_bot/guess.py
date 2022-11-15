0417a875ce569fda820103af769127db4964fe9f
CampNoNet <bob92588@gmail.com>
2022-11-15 10:18:46 -0500

    Testing SOmething with git

--- discord_bot/guess.py
+++ discord_bot/guess.py
@@ -1,38 +1,39 @@
 import random 
 import json
-import os 
+import sqlite3
+import os
 
 class GameGuess:
     def __init__(self, message):
-        #self.user = message.author
-        pass
-        #self.message = message.content
+        self.user = message.author
+        self.message = message.content
+        self.check_for_guess()
 
-   
-    def open_file(slef):
-        fhand  = 'data.json'
-        fopen = open(fhand, 'r')
-        data = json.load(fopen)
-        return data
+    def check_for_guess(self):
+        con = sqlite3.connect('discord_bot//database.db')
+        cur = con.cursor()
+        cur.execute(f"SELECT * FROM Guess WHERE user='{self.user}'")
+        data = cur.fetchall()
+        if len(data) < 1:
+            print("There is no guess for this usersss")
+            choice = self.pick_random(0, 100)
+            cur.execute(f"INSERT INTO GUESS(user, gues) VALUES('{self.user}', '{choice}')")
+            con.commit()
+            return choice
+        return data[0][2]
 
     
     def pick_random(self, min, max):
         self.min = min
         self.max = max
         choice = random.randint(self.min, self.max)
-        save = 'not code yet will be a function to save' 
-        #the guess witht he username of the perosn who made it'
         return choice
 
     def game(self):
-        data = self.open_file()
-        if 'Mesisamu' in data['guess']:
-            print("uer already has a guess in the file")
-        else:
-            print("No user found in the file")
+        pass
         
     
     def test(self):
         a = stock.get_symbol(self.message)
     
-GameGuess("test").game()
+
